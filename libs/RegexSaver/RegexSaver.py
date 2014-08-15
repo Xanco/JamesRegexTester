5e8539b3c1dd3e270e19d8a91e8757bba8058b68
James Elmour <james.elmour@comparethemarket.com>
2014-08-15 16:58:59 +0100

    #2 <Jelmour> Started adding load feature

--- libs/RegexSaver/RegexSaver.py
+++ libs/RegexSaver/RegexSaver.py
@@ -1,14 +1,27 @@
 from Tkinter import *
 
 class RegexSaver:
-	def __init__(self, passedRoot):
+	def __init__(self, passedRoot, mother):
 		self.root = passedRoot
+		self.mainUI = mother
 
 
 	def SetUpLoadWindow(self):
 		loadWindow = Tk()
-		loadWindow.grab_set()
-
+		
+		# Setup load options listbox
+		LoadOptionsFrame = Frame(loadWindow)
+		LoadOptionsFrame.pack({"fill": "x"})
+		LoadOptionScrollBar = Scrollbar(LoadOptionsFrame, orient=VERTICAL)
+		LoadOptions = Listbox(LoadOptionsFrame, yscrollcommand=LoadOptionScrollBar.set)
+		LoadOptionScrollBar.config(command=LoadOptions.yview)
+		LoadOptionScrollBar.pack({"side":"right", "fill": "y"})
+		LoadOptions.pack({"fill": "x"})
+		for item in ["1","2","3","1","2","3","1","2","3","1","2","3","1","2","3","1","2","3","1","2","3","1","2","3","1","2","3","1","2","3","1","2","3","1","2","3","1","2","3"]:
+			LoadOptions.insert(END, item)
+		
+		# Setup load button
+		#LoadButton
 
 	def Load(self):
 		self.SetUpLoadWindow()
@@ -17,8 +30,6 @@ class RegexSaver:
 		print("RegexSaverLaunched")
 
 		MenuBar = Menu(self.root)
-		#filemenu = Menu(MenuBar, tearoff=0)
 		MenuBar.add_command(label="Load", command=self.Load) # If RegexSaver.test() were called it'd have to pass self - RegexSaver.self(self) and adding () will run test immediately
-		#MenuBar.add_cascade(label="File", menu=filemenu)
 
 		self.root.config(menu=MenuBar)
--- mainUI.py
+++ mainUI.py
@@ -6,75 +6,82 @@ import ctypes
 import sys
 import os
 
-myappid = 'Jelmour.JSRegex.1' # Set ID for this program
-ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid) # Set program with this ID - also allows for icon to be displayed in taskbar
+class mainUI:
+	def __init__(self):
+		self = self
+		self.SetupUI()
 
-JsC = PyV8.JSContext() # Initialise JavaScript runner for Python
-JsC.enter() # Launch JavaScript runner
+	myappid = 'Jelmour.JSRegex.1' # Set ID for this program
+	ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid) # Set program with this ID - also allows for icon to be displayed in taskbar
 
-def TestTextAgainstRegex(Regex, TestText): # Define function TestTextAgainstRegex and get parameters Regex and TestText
-    try:
-        JsC.eval("var jsTestText = '" + str(TestText) + "'") # JavaScript runner eval (like normal JS eval) create variable with TestText
-        JsC.eval("var jsRegex = new RegExp('" + str(Regex) + "')") # Create Regex with passed Regex parameter
-        Passed =  JsC.eval("jsRegex.test(jsTestText)") # Test TestText against Regex
-        PassedLable.config(text = "") # Reset PassedLabel text to nothing
-        if Passed == True: # If regex has returned true
-            PassedLable.config(text = "Passed!", fg="#00CC00") # Set text to 'Passed!' and foreground color (text color in this instance) to green
-        else: # Otherwise
-            PassedLable.config(text = "Failed!", fg="#FF0000") # Set text to 'Failed!' and foreground color to red
-    except Exception:
-        PassedLable.config(text = "Error! " + str(sys.exc_info()[0]), fg="#0000FF")
+	JsC = PyV8.JSContext() # Initialise JavaScript runner for Python
+	JsC.enter() # Launch JavaScript runner
 
+	def TestTextAgainstRegex(self, Regex, TestText): # Define function TestTextAgainstRegex and get parameters Regex and TestText
+		try:
+			JsC.eval("var jsTestText = '" + str(TestText) + "'") # JavaScript runner eval (like normal JS eval) create variable with TestText
+			JsC.eval("var jsRegex = new RegExp('" + str(Regex) + "')") # Create Regex with passed Regex parameter
+			Passed =  JsC.eval("jsRegex.test(jsTestText)") # Test TestText against Regex
+			PassedLable.config(text = "") # Reset PassedLabel text to nothing
+			if Passed == True: # If regex has returned true
+				PassedLable.config(text = "Passed!", fg="#00CC00") # Set text to 'Passed!' and foreground color (text color in this instance) to green
+			else: # Otherwise
+				PassedLable.config(text = "Failed!", fg="#FF0000") # Set text to 'Failed!' and foreground color to red
+		except Exception:
+			PassedLable.config(text = "Error! " + str(sys.exc_info()[0]), fg="#0000FF")
+			
+	#def PassLoadValueToRegex(Regex):
+		
+	def SetupUI(self):
+		root = Tk() # Setup module for generating UI
+		root.minsize(550,75) # Set size of window to 550px by 75px
+		root.resizable(width=FALSE, height=FALSE) # Make window not resizable
+		root.title("JavaScript Regex Tester") # Set title to JavaScript Regex Tester
+		root.iconbitmap('icon.ico') # Set the window (and now taskbar) icon
 
-root = Tk() # Setup module for generating UI
-root.minsize(550,75) # Set size of window to 550px by 75px
-root.resizable(width=FALSE, height=FALSE) # Make window not resizable
-root.title("JavaScript Regex Tester") # Set title to JavaScript Regex Tester
-root.iconbitmap('icon.ico') # Set the window (and now taskbar) icon
+		#RegexSaverVar = RegexSaver(root, self)
+		#RegexSaverVar.SetUpResexSaver()
 
-RegexSaverVar = RegexSaver(root)
-RegexSaverVar.SetUpResexSaver()
+		MotherRegexFrame = Frame(root) # Create a frame (a container) inside of root (the window) and call it MotherRegexFrame
+		MotherRegexFrame.pack() # Place frame with no options
 
-MotherRegexFrame = Frame(root) # Create a frame (a container) inside of root (the window) and call it MotherRegexFrame
-MotherRegexFrame.pack() # Place frame with no options
+		RegexLableFrame = Frame(MotherRegexFrame, width = 250, height = 28) # Create a new frame inside of MotherRegexFrame with the width of 250px and height of 28px and call it RegexLableFrame
+		RegexLableFrame.pack_propagate(0) # Make sure that frame size doesn't change
+		RegexLableFrame.pack({"side": "left"}) # Place on left side of parent - a lot like float: left in CSS
 
-RegexLableFrame = Frame(MotherRegexFrame, width = 250, height = 28) # Create a new frame inside of MotherRegexFrame with the width of 250px and height of 28px and call it RegexLableFrame
-RegexLableFrame.pack_propagate(0) # Make sure that frame size doesn't change
-RegexLableFrame.pack({"side": "left"}) # Place on left side of parent - a lot like float: left in CSS
+		RegexLable = Label(RegexLableFrame, text="Enter your regex here: ") # Create a label (standard text label) inside of RegexLableFrame with the text "Enter your regex here: "
+		RegexLable.pack({"side": "bottom"}) # Place at bottom of parent frame (adds margin to top of application)
 
-RegexLable = Label(RegexLableFrame, text="Enter your regex here: ") # Create a label (standard text label) inside of RegexLableFrame with the text "Enter your regex here: "
-RegexLable.pack({"side": "bottom"}) # Place at bottom of parent frame (adds margin to top of application)
+		RegexFrame = Frame(MotherRegexFrame, width = 250, height = 28) # Create a frame like RegexLableFrame but call it RegexFrame
+		RegexFrame.pack_propagate(0) # Don't resize
+		RegexFrame.pack({"side": "right"}) # This time align to the right inside the parent
 
-RegexFrame = Frame(MotherRegexFrame, width = 250, height = 28) # Create a frame like RegexLableFrame but call it RegexFrame
-RegexFrame.pack_propagate(0) # Don't resize
-RegexFrame.pack({"side": "right"}) # This time align to the right inside the parent
+		regexEntry = Entry(RegexFrame) # Create a entry (textbox) inside of RegexFrame
+		regexEntry.pack({"fill": "x", "side": "bottom"}) # Fill the parent in the X axis and align to bottom
 
-regexEntry = Entry(RegexFrame) # Create a entry (textbox) inside of RegexFrame
-regexEntry.pack({"fill": "x", "side": "bottom"}) # Fill the parent in the X axis and align to bottom
+		# This part of the UI is the same as before, except with different text and variable names
+		MotherTestTextFrame = Frame(root)
+		MotherTestTextFrame.pack()
 
-# This part of the UI is the same as before, except with different text and variable names
-MotherTestTextFrame = Frame(root)
-MotherTestTextFrame.pack()
+		TestTextLableFrame = Frame(MotherTestTextFrame, width = 250, height = 28)
+		TestTextLableFrame.pack_propagate(0)
+		TestTextLableFrame.pack({"side": "left"})
 
-TestTextLableFrame = Frame(MotherTestTextFrame, width = 250, height = 28)
-TestTextLableFrame.pack_propagate(0)
-TestTextLableFrame.pack({"side": "left"})
+		TestTextLable = Label(TestTextLableFrame, text="Enter the text you want to test here: ")
+		TestTextLable.pack({"side": "bottom"})
 
-TestTextLable = Label(TestTextLableFrame, text="Enter the text you want to test here: ")
-TestTextLable.pack({"side": "bottom"})
+		TestTextFrame = Frame(MotherTestTextFrame, width = 250, height = 28)
+		TestTextFrame.pack_propagate(0)
+		TestTextFrame.pack({"side": "right"})
 
-TestTextFrame = Frame(MotherTestTextFrame, width = 250, height = 28)
-TestTextFrame.pack_propagate(0)
-TestTextFrame.pack({"side": "right"})
+		TestTextEntry = Entry(TestTextFrame)
+		TestTextEntry.pack({"fill": "x", "side": "bottom"})
 
-TestTextEntry = Entry(TestTextFrame)
-TestTextEntry.pack({"fill": "x", "side": "bottom"})
+		# Adding the all important button
+		CheckText = Button(root, text="Test text against regex", command= lambda: self.TestTextAgainstRegex(regexEntry.get(), TestTextEntry.get())) # Create button with the text "Test text against regex" that onclick fired TestTextAgainstRegex and passes the value of regexEntry and TestTextEntry
+		CheckText.pack()
 
-# Adding the all important button
-CheckText = Button(root, text="Test text against regex", command= lambda: TestTextAgainstRegex(regexEntry.get(), TestTextEntry.get())) # Create button with the text "Test text against regex" that onclick fired TestTextAgainstRegex and passes the value of regexEntry and TestTextEntry
-CheckText.pack()
-
-PassedLable = Label(root, text="") # Set up the label that displayed whether the regex has passes, failed or produced an error
-PassedLable.pack()
-
+		PassedLable = Label(root, text="") # Set up the label that displayed whether the regex has passes, failed or produced an error
+		PassedLable.pack()
+root = Tk()
 mainloop() # Start the program
